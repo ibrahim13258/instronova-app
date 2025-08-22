e1f7c9c3e92a5934b000f412ef8fd4ea60ec7e3e
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-08-22 12:34:44 +0530

    Create Runner-Bridging-Header.h

---
+++ ios/Runner/Runner-Bridging-Header.h
@@ -0,0 +1,25 @@
+//
+//  Runner-Bridging-Header.h
+//  Runner
+//
+//  Created by Instanova Team
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+// Firebase
+#import <Firebase/Firebase.h>
+
+// Facebook
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+// Google SignIn
+#import <GoogleSignIn/GoogleSignIn.h>
+
+// Camera & Photos
+#import <AVFoundation/AVFoundation.h>
+#import <Photos/Photos.h>
+
+// Notifications
+#import <UserNotifications/UserNotifications.h>
