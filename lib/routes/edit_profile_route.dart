150b98d47e9993cff3cb11310f154e6efa39b7aa
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 12:54:24 +0530

    Create edit_profile_route.dart

---
+++ lib/routes/edit_profile_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/edit_profile_screen.dart';
+
+class EditProfileRoute {
+  // Route path
+  static const String path = '/edit_profile';
+
+  // GetPage for EditProfile screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const EditProfileScreen(),
+    transition: Transition.rightToLeft, // slide transition
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
