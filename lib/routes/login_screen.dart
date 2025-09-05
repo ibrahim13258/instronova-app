24d75af4ec2288d6eabd2fa427dfbedece8f1309
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 12:27:23 +0530

    Create login_screen.dart

---
+++ lib/routes/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import '../screens/login_screen.dart';
+
+class LoginRoute {
+  static const String path = '/login';
+
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const LoginScreen(),
+    transition: Transition.fadeIn,
+    // You can add bindings or middlewares here if needed
+  );
+}
