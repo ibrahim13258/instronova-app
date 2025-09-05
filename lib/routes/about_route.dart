798b8fc57ba98629d692097bb12bc919b38f42c8
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 14:46:40 +0530

    Create about_route.dart

---
+++ lib/routes/about_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/about_screen.dart';
+
+class AboutRoute {
+  // Route path
+  static const String path = '/about';
+
+  // GetPage for About screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const AboutScreen(),
+    transition: Transition.rightToLeft, // slide transition
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
