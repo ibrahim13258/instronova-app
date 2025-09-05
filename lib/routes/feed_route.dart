bc1eba6a65a53b2dbefa5cbb1c6d3722b2d4ce65
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 12:45:41 +0530

    Create feed_route.dart

---
+++ lib/routes/feed_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/feed_screen.dart';
+
+class FeedRoute {
+  // Route path
+  static const String path = '/feed';
+
+  // GetPage for Feed screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const FeedScreen(),
+    transition: Transition.rightToLeft, // Slide effect
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
