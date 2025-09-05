01feb23935c42e0e4c929f4f51da65ef82bd230b
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 12:28:11 +0530

    Create signup_screen.dart

---
+++ lib/routes/screens/signup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/signup_screen.dart';
+
+class SignupRoute {
+  // Route path
+  static const String path = '/signup';
+
+  // GetPage for Signup screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const SignupScreen(),
+    transition: Transition.rightToLeft, // Smooth slide effect
+    transitionDuration: const Duration(milliseconds: 400),
+  );
+}
