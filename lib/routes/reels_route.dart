264b1620d473d06eee9d5f5d83e40cac5535207f
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 12:46:23 +0530

    Create reels_route.dart

---
+++ lib/routes/reels_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/reels_screen.dart';
+
+class ReelsRoute {
+  // Route path
+  static const String path = '/reels';
+
+  // GetPage for Reels screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const ReelsScreen(),
+    transition: Transition.downToUp, // vertical slide transition
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
