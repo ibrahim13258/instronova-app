7019bb02532ae8723835ec6801d1bf8bbe2e59b4
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 22:15:40 +0530

    Create app_strings.dart

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,61 @@
+class AppStrings {
+  // General
+  static const String appName = "Instronova";
+  static const String ok = "OK";
+  static const String cancel = "Cancel";
+  static const String submit = "Submit";
+  static const String edit = "Edit";
+  static const String delete = "Delete";
+  static const String save = "Save";
+  static const String update = "Update";
+  static const String search = "Search";
+  static const String logout = "Logout";
+
+  // Auth Screens
+  static const String loginTitle = "Login to your account";
+  static const String signupTitle = "Create a new account";
+  static const String forgotPassword = "Forgot your password?";
+  static const String emailHint = "Email";
+  static const String passwordHint = "Password";
+  static const String confirmPasswordHint = "Confirm Password";
+  static const String loginButton = "Login";
+  static const String signupButton = "Sign Up";
+  static const String resetPasswordButton = "Reset Password";
+
+  // Profile
+  static const String completeProfileTitle = "Complete Your Profile";
+  static const String profileEditTitle = "Edit Profile";
+  static const String followers = "Followers";
+  static const String following = "Following";
+  static const String posts = "Posts";
+
+  // Posts & Feed
+  static const String addPost = "Add Post";
+  static const String postCaptionHint = "Write a caption...";
+  static const String like = "Like";
+  static const String comment = "Comment";
+  static const String share = "Share";
+  static const String savePost = "Save Post";
+
+  // Notifications
+  static const String notifications = "Notifications";
+  static const String noNotifications = "No new notifications";
+
+  // Settings
+  static const String settings = "Settings";
+  static const String accountSettings = "Account Settings";
+  static const String privacySettings = "Privacy Settings";
+  static const String securitySettings = "Security Settings";
+  static const String helpCenter = "Help Center";
+  static const String about = "About";
+
+  // Errors
+  static const String errorOccurred = "Something went wrong!";
+  static const String noInternet = "No internet connection";
+  static const String invalidEmail = "Invalid email address";
+  static const String passwordTooShort = "Password must be at least 6 characters";
+
+  // Misc
+  static const String storyViewer = "Stories";
+  static const String multiAccountSwitcher = "Switch Account";
+}
