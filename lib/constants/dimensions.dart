187215ca6d5790c1f16a14653f769c9f9dd58e38
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 22:27:03 +0530

    Create dimensions.dart

---
+++ lib/constants/dimensions.dart
@@ -0,0 +1,58 @@
+// lib/constants/dimensions.dart
+
+import 'package:flutter/material.dart';
+
+class Dimensions {
+  // General padding & margin
+  static const double paddingSmall = 4.0;
+  static const double paddingMedium = 8.0;
+  static const double paddingLarge = 16.0;
+  static const double paddingXLarge = 24.0;
+  static const double paddingXXLarge = 32.0;
+
+  static const double marginSmall = 4.0;
+  static const double marginMedium = 8.0;
+  static const double marginLarge = 16.0;
+  static const double marginXLarge = 24.0;
+  static const double marginXXLarge = 32.0;
+
+  // Border radius
+  static const double borderRadiusSmall = 4.0;
+  static const double borderRadiusMedium = 8.0;
+  static const double borderRadiusLarge = 16.0;
+  static const double borderRadiusXLarge = 32.0;
+  static const double borderRadiusCircle = 50.0;
+
+  // Icon sizes
+  static const double iconSmall = 16.0;
+  static const double iconMedium = 24.0;
+  static const double iconLarge = 32.0;
+  static const double iconXLarge = 48.0;
+
+  // Text field height
+  static const double textFieldHeight = 48.0;
+
+  // AppBar & Toolbar heights
+  static const double appBarHeight = 56.0;
+  static const double bottomNavBarHeight = 60.0;
+
+  // Card & container sizes
+  static const double cardElevation = 2.0;
+  static const double cardPadding = 12.0;
+
+  // Avatar sizes
+  static const double avatarSmall = 24.0;
+  static const double avatarMedium = 40.0;
+  static const double avatarLarge = 64.0;
+
+  // Spacing widgets
+  static const double spaceTiny = 2.0;
+  static const double spaceSmall = 4.0;
+  static const double spaceMedium = 8.0;
+  static const double spaceLarge = 16.0;
+  static const double spaceXLarge = 24.0;
+
+  // Max widths for responsive design
+  static const double maxScreenWidth = 1200.0;
+  static const double maxContentWidth = 800.0;
+}
