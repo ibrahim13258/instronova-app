c8660d238103b22e449cc3f106b0c1bc9fcc68c5
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 22:17:22 +0530

    Create app_images.dart

---
+++ lib/constants/app_images.dart
@@ -0,0 +1,34 @@
+class AppImages {
+  // Splash & Onboarding
+  static const String splashLogo = 'assets/images/splash_logo.png';
+  static const String onboarding1 = 'assets/images/onboarding1.png';
+  static const String onboarding2 = 'assets/images/onboarding2.png';
+  static const String onboarding3 = 'assets/images/onboarding3.png';
+
+  // Icons
+  static const String iconHome = 'assets/icons/home.png';
+  static const String iconFeed = 'assets/icons/feed.png';
+  static const String iconReels = 'assets/icons/reels.png';
+  static const String iconSearch = 'assets/icons/search.png';
+  static const String iconNotifications = 'assets/icons/notifications.png';
+  static const String iconChat = 'assets/icons/chat.png';
+  static const String iconProfile = 'assets/icons/profile.png';
+  static const String iconAddPost = 'assets/icons/add_post.png';
+  static const String iconSettings = 'assets/icons/settings.png';
+  static const String iconBack = 'assets/icons/back.png';
+  static const String iconLike = 'assets/icons/like.png';
+  static const String iconComment = 'assets/icons/comment.png';
+  static const String iconShare = 'assets/icons/share.png';
+  static const String iconSave = 'assets/icons/save.png';
+  static const String iconVerified = 'assets/icons/verified.png';
+
+  // Placeholder Images
+  static const String placeholderProfile = 'assets/images/placeholder_profile.png';
+  static const String placeholderPost = 'assets/images/placeholder_post.png';
+  static const String placeholderStory = 'assets/images/placeholder_story.png';
+  static const String placeholderReels = 'assets/images/placeholder_reels.png';
+
+  // Other Images
+  static const String emptyState = 'assets/images/empty_state.png';
+  static const String errorState = 'assets/images/error_state.png';
+}
