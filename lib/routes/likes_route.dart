c939bfe3814dc7ae0ee82c7bc771ef4885e64cb4
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 14:49:45 +0530

    Create likes_route.dart

---
+++ lib/routes/likes_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/likes_screen.dart';
+
+class LikesRoute {
+  // Route path
+  static const String path = '/likes';
+
+  // GetPage for Likes screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const LikesScreen(),
+    transition: Transition.rightToLeft, // slide transition
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
