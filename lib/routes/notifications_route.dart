98791a0974b37f3ad9307399deea0274711c90a1
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 12:47:44 +0530

    Create notifications_route.dart

---
+++ lib/routes/notifications_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/notifications_screen.dart';
+
+class NotificationsRoute {
+  // Route path
+  static const String path = '/notifications';
+
+  // GetPage for Notifications screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const NotificationsScreen(),
+    transition: Transition.rightToLeft, // horizontal slide transition
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
