8a0384778dcb7952517a555a7ad907ea9d235c5e
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 14:47:47 +0530

    Create saved_posts_route.dart

---
+++ lib/routes/saved_posts_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/saved_posts_screen.dart';
+
+class SavedPostsRoute {
+  // Route path
+  static const String path = '/saved_posts';
+
+  // GetPage for Saved Posts screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const SavedPostsScreen(),
+    transition: Transition.rightToLeft, // slide transition
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
