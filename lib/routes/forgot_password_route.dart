df70aba0c34646a84086b8e81e698d527293efeb
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 12:30:14 +0530

    Create forgot_password_route.dart

---
+++ lib/routes/forgot_password_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/forgot_password_screen.dart';
+
+class ForgotPasswordRoute {
+  // Route path
+  static const String path = '/forgot_password';
+
+  // GetPage for Forgot Password screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const ForgotPasswordScreen(),
+    transition: Transition.downToUp, // Slide from bottom
+    transitionDuration: const Duration(milliseconds: 400),
+  );
+}
