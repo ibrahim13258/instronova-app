e676de0b227d5788531e3c7a09a240c230834017
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 12:42:21 +0530

    Create complete_profile_route.dart

---
+++ lib/routes/complete_profile_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/complete_profile_screen.dart';
+
+class CompleteProfileRoute {
+  // Route path
+  static const String path = '/complete_profile';
+
+  // GetPage for Complete Profile screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const CompleteProfileScreen(),
+    transition: Transition.cupertino, // iOS style transition
+    transitionDuration: const Duration(milliseconds: 400),
+  );
+}
