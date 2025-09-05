a371f4013291d846584e855f16fe67a7d9452ccd
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 12:51:19 +0530

    Create story_viewer_route.dart

---
+++ lib/routes/story_viewer_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/story_viewer_screen.dart';
+
+class StoryViewerRoute {
+  // Route path
+  static const String path = '/story_viewer';
+
+  // GetPage for Story Viewer screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const StoryViewerScreen(),
+    transition: Transition.fade, // fade transition for stories
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
