3598f852f3e68ea51aa0a91d947cd0bf487ad7b5
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 13:15:05 +0530

    Create settings_route.dart

---
+++ lib/routes/settings_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/settings_screen.dart';
+
+class SettingsRoute {
+  // Route path
+  static const String path = '/settings';
+
+  // GetPage for Settings screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const SettingsScreen(),
+    transition: Transition.rightToLeft, // slide transition
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
