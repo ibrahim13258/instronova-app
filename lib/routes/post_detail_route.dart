933dfa04713ac2f2b95093ba78ac79622f6649ab
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 12:52:59 +0530

    Create  post_detail_route.dart

---
+++ lib/routes/post_detail_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/post_detail_screen.dart';
+
+class PostDetailRoute {
+  // Route path
+  static const String path = '/post_detail';
+
+  // GetPage for Post Detail screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const PostDetailScreen(),
+    transition: Transition.rightToLeft, // slide transition for detail view
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
