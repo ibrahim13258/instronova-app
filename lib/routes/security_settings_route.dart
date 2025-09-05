9275ea130b1ec995ddfba43990213dfb51dee606
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 14:45:37 +0530

    Create security_settings_route.dart

---
+++ lib/routes/security_settings_route.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import '../screens/security_settings_screen.dart';
+
+class SecuritySettingsRoute {
+  // Route path
+  static const String path = '/security_settings';
+
+  // GetPage for Security Settings screen
+  static final GetPage page = GetPage(
+    name: path,
+    page: () => const SecuritySettingsScreen(),
+    transition: Transition.rightToLeft, // slide transition
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
