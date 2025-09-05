7902f83f5be13bbf2bb9f3612058abdd119d5622
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 22:29:47 +0530

    Update regex_patterns.dart

--- lib/constants/regex_patterns.dart
+++ lib/constants/regex_patterns.dart
@@ -1,36 +1,34 @@
 class RegexPatterns {
   // Email validation
   static final RegExp email = RegExp(
-    r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+    r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+    caseSensitive: false,
   );
 
-  // Phone number validation (10 digit, India)
+  // Phone number validation (Indian +91 format or general)
   static final RegExp phone = RegExp(
-    r'^[6-9]\d{9}$',
+    r'^(?:\+91|0)?[6-9]\d{9}$',
   );
 
-  // Username validation: 3-15 chars, letters, numbers, underscores
-  static final RegExp username = RegExp(
-    r'^[a-zA-Z0-9_]{3,15}$',
-  );
-
-  // Password validation: Minimum 8 chars, at least 1 upper, 1 lower, 1 digit, 1 special char
+  // Password validation
+  // Minimum 8 characters, at least 1 uppercase, 1 lowercase, 1 number, 1 special char
   static final RegExp password = RegExp(
     r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
   );
 
+  // Username validation
+  // Letters, numbers, underscore, 3-20 characters
+  static final RegExp username = RegExp(
+    r'^[a-zA-Z0-9_]{3,20}$',
+  );
+
   // URL validation
   static final RegExp url = RegExp(
-    r'^(https?:\/\/)?([\w\-])+\.{1}([a-zA-Z]{2,63})([\/\w\-.?=&]*)*\/?$',
+    r'^(https?:\/\/)?([\w\-])+\.{1}([a-zA-Z]{2,63})([\/\w\-\.\?=&]*)*\/?$',
   );
 
-  // Only letters validation
-  static final RegExp onlyLetters = RegExp(
-    r'^[a-zA-Z]+$',
-  );
-
-  // Only numbers validation
-  static final RegExp onlyNumbers = RegExp(
-    r'^\d+$',
+  // OTP validation (4-6 digits)
+  static final RegExp otp = RegExp(
+    r'^\d{4,6}$',
   );
 }
