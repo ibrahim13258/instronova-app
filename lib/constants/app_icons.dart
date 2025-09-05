33004edf8ff6c3faf639d856a92ee08d18bb9d11
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-05 22:19:07 +0530

    Create app_icons.dart

---
+++ lib/constants/app_icons.dart
@@ -0,0 +1,35 @@
+// lib/constants/app_icons.dart
+
+class AppIcons {
+  // Bottom Navigation Icons
+  static const String home = 'assets/icons/home.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String reels = 'assets/icons/reels.svg';
+  static const String shop = 'assets/icons/shop.svg';
+  static const String profile = 'assets/icons/profile.svg';
+
+  // Action Icons
+  static const String like = 'assets/icons/like.svg';
+  static const String comment = 'assets/icons/comment.svg';
+  static const String share = 'assets/icons/share.svg';
+  static const String bookmark = 'assets/icons/bookmark.svg';
+  static const String addPost = 'assets/icons/add_post.svg';
+
+  // Story & Reels Icons
+  static const String storyAdd = 'assets/icons/story_add.svg';
+  static const String storyPlay = 'assets/icons/story_play.svg';
+  static const String reelsPlay = 'assets/icons/reels_play.svg';
+
+  // Profile & Settings Icons
+  static const String settings = 'assets/icons/settings.svg';
+  static const String editProfile = 'assets/icons/edit_profile.svg';
+  static const String followers = 'assets/icons/followers.svg';
+  static const String following = 'assets/icons/following.svg';
+  static const String logout = 'assets/icons/logout.svg';
+
+  // Miscellaneous
+  static const String verified = 'assets/icons/verified.svg';
+  static const String searchResult = 'assets/icons/search_result.svg';
+  static const String notification = 'assets/icons/notification.svg';
+  static const String message = 'assets/icons/message.svg';
+}
