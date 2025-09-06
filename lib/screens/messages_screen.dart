250fe5da142ca2e164a2bb6daa8083ff37ed686b
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-09-06 11:11:02 +0530

    Update messages_screen.dart

--- lib/screens/messages_screen.dart
+++ lib/screens/messages_screen.dart
@@ -1,58 +1,18 @@
 // File: screens/messages_screen.dart
 import 'package:flutter/material.dart';
+import 'chat_detail_screen.dart';
 
 class MessagesScreen extends StatelessWidget {
-  MessagesScreen({Key? key}) : super(key: key);
-
-  final List<Map<String, dynamic>> chatUsers = List.generate(
-    15,
+  final List<Map<String, String>> chats = List.generate(
+    10,
     (index) => {
       "username": "user_$index",
-      "lastMessage": "Last message from user_$index",
-      "time": "${index + 1}:${(index * 3) % 60} PM",
       "profileImage": "https://i.pravatar.cc/150?img=$index",
-      "isOnline": index % 3 == 0,
+      "lastMessage": "Last message from user_$index",
     },
   );
 
-  Widget _buildChatTile(Map<String, dynamic> user) {
-    return ListTile(
-      leading: Stack(
-        children: [
-          CircleAvatar(
-            radius: 24,
-            backgroundImage: NetworkImage(user['profileImage']),
-          ),
-          if (user['isOnline'])
-            Positioned(
-              bottom: 0,
-              right: 0,
-              child: Container(
-                width: 12,
-                height: 12,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.white, width: 2),
-                ),
-              ),
-            ),
-        ],
-      ),
-      title: Text(
-        user['username'],
-        style: const TextStyle(fontWeight: FontWeight.bold),
-      ),
-      subtitle: Text(user['lastMessage']),
-      trailing: Text(
-        user['time'],
-        style: const TextStyle(color: Colors.grey, fontSize: 12),
-      ),
-      onTap: () {
-        // Navigate to chat detail screen
-      },
-    );
-  }
+  MessagesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -60,27 +20,47 @@ class MessagesScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Messages"),
         backgroundColor: isDark ? Colors.black : Colors.white,
-        elevation: 1,
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.edit,
-                color: isDark ? Colors.white : Colors.black),
-            onPressed: () {
-              // Compose new message
-            },
-          ),
-        ],
+        title: Text(
+          "Messages",
+          style: TextStyle(color: isDark ? Colors.white : Colors.black),
+        ),
       ),
       body: ListView.separated(
-        itemCount: chatUsers.length,
-        separatorBuilder: (context, index) => const Divider(
-          height: 0,
-          indent: 70,
-        ),
-        itemBuilder: (context, index) => _buildChatTile(chatUsers[index]),
+        itemCount: chats.length,
+        separatorBuilder: (_, __) => const Divider(height: 1),
+        itemBuilder: (context, index) {
+          final chat = chats[index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(chat["profileImage"]!),
+            ),
+            title: Text(
+              chat["username"]!,
+              style: TextStyle(
+                color: isDark ? Colors.white : Colors.black,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            subtitle: Text(
+              chat["lastMessage"]!,
+              style: TextStyle(
+                color: isDark ? Colors.white70 : Colors.black54,
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => ChatDetailScreen(
+                    username: chat["username"]!,
+                    userProfile: chat["profileImage"]!,
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
