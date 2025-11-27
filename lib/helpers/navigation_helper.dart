b83045943d0b3353fdd8dc6a1315c725e9e614e3
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-11-27 14:26:31 +0530

    Add files via upload

--- lib/helpers/navigation_helper.dart
+++ lib/helpers/navigation_helper.dart
@@ -4,43 +4,43 @@ import 'package:get/get.dart';
 class NavigationHelper {
   /// Push to new screen
   static void push(Widget page) {
-    Get.to(() => page);
+// TODO: Replace GetX navigation: Get.to(() => page);
   }
 
   /// Replace current screen with new screen
   static void pushReplacement(Widget page) {
-    Get.off(() => page);
+// TODO: Replace GetX navigation: Get.off(() => page);
   }
 
   /// Remove all previous screens and go to new screen
   static void pushAndRemoveUntil(Widget page) {
-    Get.offAll(() => page);
+// TODO: Replace GetX navigation: Get.offAll(() => page);
   }
 
   /// Pop current screen
   static void pop([dynamic result]) {
-    if (Get.isOverlaysOpen || Get.canPop()) {
-      Get.back(result: result);
+// TODO: Replace GetX navigation: if (Get.isOverlaysOpen || Get.canPop()) {
+// TODO: Replace GetX navigation: Get.back(result: result);
     }
   }
 
   /// Pop until specific route name
   static void popUntil(String routeName) {
-    Get.until((route) => route.settings.name == routeName);
+// TODO: Replace GetX navigation: Get.until((route) => route.settings.name == routeName);
   }
 
   /// Navigate to named route with optional arguments
   static void navigateTo(String routeName, {dynamic arguments}) {
-    Get.toNamed(routeName, arguments: arguments);
+// TODO: Replace GetX navigation: Get.toNamed(routeName, arguments: arguments);
   }
 
   /// Replace with named route
   static void navigateReplacement(String routeName, {dynamic arguments}) {
-    Get.offNamed(routeName, arguments: arguments);
+// TODO: Replace GetX navigation: Get.offNamed(routeName, arguments: arguments);
   }
 
   /// Remove all previous routes and go to named route
   static void navigateAndRemoveUntil(String routeName, {dynamic arguments}) {
-    Get.offAllNamed(routeName, arguments: arguments);
+// TODO: Replace GetX navigation: Get.offAllNamed(routeName, arguments: arguments);
   }
 }
