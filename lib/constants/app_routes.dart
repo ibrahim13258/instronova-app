03d4b2835a93ec32ad5b27d1757cc1c108b6da64
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-11-27 12:44:06 +0530

    Add files via upload

--- lib/constants/app_routes.dart
+++ lib/constants/app_routes.dart
@@ -1,4 +1,4 @@
-import 'package:get/get.dart';
+// GetX removed for Provider consistency
 import '../screens/splash_screen.dart';
 import '../screens/login_screen.dart';
 import '../screens/signup_screen.dart';
