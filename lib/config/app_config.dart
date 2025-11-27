7b2bb55373bf68f6725c4e9b7779f0d722832c2c
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-11-27 13:05:23 +0530

    Update app_config.dart

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,3 +1,13 @@
-class AppConfig {
+ class AppConfig {
+  /// Base API URL (main backend)
   static const String baseUrl = "https://api.instronova.in/v1";
+
+  /// Base file URL (e.g., images / media loading)
+  static const String mediaBaseUrl = "https://cdn.instronova.in/";
+
+  /// WebSocket connection (अगर future में realtime चाहिए)
+  static const String socketUrl = "wss://api.instronova.in/ws";
+
+  /// Current environment
+  static const String environment = "production";
 }
